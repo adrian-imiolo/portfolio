39c601a72c038fbcf7d8fdbe59908963a8332a4f
Adrian Imioło <adrian.imiolo11@gmail.com>
2023-06-27 15:57:07 +0200

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/resume-adrian-imiolo.pdf";
+import pdf from "../../Assets/../Assets/adrian-imiolo-resume.pdf";
 import cv from "../../Assets/../Assets/cv.png";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
