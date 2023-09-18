ef8734a21a578c5f18d143fed7733549d05e2b33
Adrian Imioło <adrian.imiolo11@gmail.com>
2023-09-18 20:27:18 +0200

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -13,9 +13,6 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <AiFillHtml5 />
